470cb6ee6464f8d5fb6068f097f93db29e812258
mahiro <65147170+mahiro0802@users.noreply.github.com>
2022-11-24 15:06:18 +0900

    初期段階

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace kimonoちゃんストレスメーター
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.リセットToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.フォント変更ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 99);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(352, 43);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem,
+            this.設定ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(376, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.リセットToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.終了XToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // リセットToolStripMenuItem
+            // 
+            this.リセットToolStripMenuItem.Name = "リセットToolStripMenuItem";
+            this.リセットToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.リセットToolStripMenuItem.Text = "リセット";
+            this.リセットToolStripMenuItem.Click += new System.EventHandler(this.リセットToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(110, 6);
+            // 
+            // 終了XToolStripMenuItem
+            // 
+            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.終了XToolStripMenuItem.Text = "終了(&X)";
+            this.終了XToolStripMenuItem.Click += new System.EventHandler(this.終了XToolStripMenuItem_Click);
+            // 
+            // 設定ToolStripMenuItem
+            // 
+            this.設定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.フォント変更ToolStripMenuItem});
+            this.設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
+            this.設定ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.設定ToolStripMenuItem.Text = "設定";
+            // 
+            // フォント変更ToolStripMenuItem
+            // 
+            this.フォント変更ToolStripMenuItem.Name = "フォント変更ToolStripMenuItem";
+            this.フォント変更ToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.フォント変更ToolStripMenuItem.Text = "フォント変更";
+            this.フォント変更ToolStripMenuItem.Click += new System.EventHandler(this.フォント変更ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 154);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ストレスメーター(β)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem リセットToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem フォント変更ToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kimonoちゃんストレスメーター
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ロード時に前回設定したフォントを読み込む
+            label2.Font = Properties.Settings.Default.font;
+        }
+
+        private void フォント変更ToolStripMenuItem_Click(object sender, EventArgs e)
+        {   
+            
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                //フォント変更
+                label2.Font = fontDialog1.Font;
+                //設定ファイルの変更・保存
+                Properties.Settings.Default.font = fontDialog1.Font;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        
+        private void Form1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            //押されたキーがスペースキーか調べる
+            if(e.KeyCode == Keys.Space)
+            {
+                //新しい数値
+                int new_score = int.Parse(label2.Text) + 1;
+                //新しい数値をテキストにセットする
+                label2.Text = new_score.ToString();
+
+                //文字色の変更処理
+                
+                //旧式
+                /*if(new_score >= 50)
+                {
+                    if (new_score >= 100)
+                    {
+                        label2.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        label2.ForeColor = Color.DarkOrange;
+                    }
+
+                }*/
+
+                //新式、初めてのswitch式
+                switch (new_score)
+                {
+                    case 50:
+                        label2.ForeColor = Color.DarkOrange;
+                        break;
+                    case 100:
+                        label2.ForeColor = Color.Red;
+                        break;
+                }
+                
+                //プログレスバーのvalueが100を超えないようにする
+                if (progressBar1.Value != 100)
+                {
+                    //プログレスバーの数字にプラス1する
+                    progressBar1.Value = progressBar1.Value + 1;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //終了時にダイアログの作成
+            DialogResult dialogResult;
+            dialogResult = MessageBox.Show("終了しますか？", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            //ダイアログ内でキャンセルされた場合終了処理をキャンセル
+            if(dialogResult == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void リセットToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //すべてをリセット
+            label2.Text = "0";
+            label2.ForeColor = Color.Black;
+            progressBar1.Value = 0;
+        }
+
+        private void 終了XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //終了
+            this.Close();
+        }
+    }
+}
